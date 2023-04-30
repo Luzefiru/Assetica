92206639bd4e768bcec8c04a8db8597f91da5bef
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-30 18:08:31 +0800

    feat: establish MongoDB connection and add middleware

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-// Modules
-const express = require('express');
-const path = require('path');
+/* Modules */
 require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
-// Initialization
+/* Initialization */
 const app = express();
 const port = 3000,
   host = 'localhost';
@@ -11,11 +12,17 @@ app.listen(port, host, () => {
   console.log(`Serving in http://${host}:${port}`);
 });
 
-// Settings
+/* Database Connection */
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.CONNECTION_STRING);
+
+/* Settings */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Middleware
+/* Middleware */
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
